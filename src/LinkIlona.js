e0d0402bab64913b028241eadce1e6d47dc03bd9
Velcipher <ilona.mogilnikova@gmail.com>
2022-08-21 02:07:49 -0400

    Update LinkIlona.js

--- src/LinkIlona.js
+++ src/LinkIlona.js
@@ -6,13 +6,13 @@ export default function LinkIlona (){
     <span>This project was coded by Ilona Mohilnikova and is open-sourced on 
         <a
           
-          href="https://github.com/Velcipher/weather_app_vanilla"
+          href="https://github.com/Velcipher/dictionary-react"
           target="_blank"
           rel="noreferrer"
           > GitHub </a>
         and hosted on
         <a
-          href="https://app.netlify.com/sites/cool-empanada-6e1517/overview"
+          href="https://lucky-crostata-b0319d.netlify.app/"
           target="_blank"
           rel="noreferrer"
           > Netlify </a></span>
